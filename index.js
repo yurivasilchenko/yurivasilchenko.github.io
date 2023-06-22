3f68e9d505ea16c40d8181e602e20c95b7e19228
YourPuppeteer <mr.dovakin1998@mail.ru>
2023-06-22 14:43:42 +0400

    Targets amount increase and decrease functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const gameContainer = document.getElementById('game-container');
-const targets = document.querySelectorAll('.target');
+/*const targets = document.querySelectorAll('.target');*/
+let targets = Array.from(document.getElementsByClassName('target'));
 const timeRemaining = document.getElementById('time-remaining');
 const currentScore = document.getElementById('current-score');
 const finalScore = document.getElementById('final-score')
@@ -7,10 +8,11 @@ const startButton = document.getElementById('start-button');
 const retryButton = document.getElementById('retry-button');
 const scoreContainer = document.getElementById('score-container')
 
-const targetArray = Array.from(targets);
+
+let targetArray = Array.from(targets);
 
 let score = 0;
-let remainingTime = 60;
+let remainingTime = 5;
 let isGameStarted = false;
 let countdownInterval;
 
@@ -25,10 +27,13 @@ function incrementScore(event) {
         return;
     }
 
+
     const clickedTarget = event.target;
 
     if (targetArray.includes(clickedTarget)) {
+
         score++;
+        console.log(clickedTarget)
         currentScore.textContent = score.toString();
         repositionTarget(clickedTarget);
 
@@ -119,11 +124,13 @@ function startGame() {
     startButton.style.display = 'none';
 
     targets.forEach(target => {
+
         target.style.display = 'block';
         target.addEventListener('mousedown', incrementScore);
         repositionTarget(target);
         targetClickTimes.set(target, Date.now());
     });
+    targets = Array.from(document.getElementsByClassName('target'));
 
     countdownInterval = setInterval(countDown, 1000);
 }
@@ -132,14 +139,17 @@ function startGame() {
 function endGame() {
     isGameStarted = false;
     finalScore.textContent = score.toString();
-    targets.forEach(target => gameContainer.removeChild(target));
+    targets.forEach(target => {
+        target.style.display = 'none';
+        target.removeEventListener('mousedown', incrementScore);
+    });
     scoreContainer.style.display = 'block';
 
 }
 
 function restartGame() {
     score = 0;
-    remainingTime = 60;
+    remainingTime = 5;
     finalScore.textContent = '';
     startButton.style.display = 'block';
 
@@ -150,12 +160,16 @@ function restartGame() {
         gameContainer.appendChild(target); // Append each target back to the game container
     });
 
+    targets = Array.from(document.getElementsByClassName('target'));
+    console.log(targets)
+
     scoreContainer.style.display = 'none';
     resetTargetTimeout();
 }
 
 // after time is out, this function removes targets and calls for endGame function, also it repositionsTarget after 4 seconds if it is not clicked
 function countDown() {
+
     remainingTime--;
     timeRemaining.textContent = remainingTime.toString();
 
@@ -184,3 +198,82 @@ function resetTargetTimeout() {
     targetClickTimes.clear();
 }
 
+//Test
+
+const decreaseTargetsBtn = document.getElementById('decrease-targets');
+const increaseTargetsBtn = document.getElementById('increase-targets');
+const numTargetsSpan = document.getElementById('num-targets');
+
+let numTargets = 4; // Initial number of targets
+numTargetsSpan.textContent = numTargets.toString(); // Set initial value
+
+// Function to increase the number of targets
+
+
+
+
+// Function to increase the number of targets
+// Function to increase the number of targets
+function increaseTargets() {
+
+
+    if (isGameStarted) {
+        return;
+    }
+
+    numTargets++;
+    numTargetsSpan.textContent = numTargets.toString();
+    const gameContainer = document.getElementById('game-container');
+    const targetDivs = gameContainer.getElementsByClassName('target');
+    if (targetDivs.length < numTargets) {
+        const newTarget = document.createElement('div');
+        newTarget.id = `target${targetDivs.length + 1}`;
+        newTarget.className = 'target';
+        newTarget.dataset.clicked = 'false';
+        newTarget.dataset.timerId = '';
+        newTarget.style.display = 'none';
+        gameContainer.appendChild(newTarget);
+
+        targets = Array.from(document.getElementsByClassName('target')); // Update targets array
+        targetArray = Array.from(targets); // Update targetArray
+        console.log(targets)
+
+        // Call repositionTarget for each newly added target
+        targets.slice(-1 * (numTargets - targetDivs.length)).forEach(target => {
+            target.addEventListener('mousedown', incrementScore); // Attach the event listener
+            repositionTarget(target);
+        });
+    }
+}
+
+// Function to decrease the number of targets
+function decreaseTargets() {
+
+    if (isGameStarted) {
+        return;
+    }
+
+
+
+    if (numTargets > 1) {
+        numTargets--;
+        numTargetsSpan.textContent = numTargets.toString();
+        const gameContainer = document.getElementById('game-container');
+        const targetDivs = gameContainer.getElementsByClassName('target');
+
+
+        if (targetDivs.length > numTargets) {
+            gameContainer.removeChild(targetDivs[targetDivs.length - 1]);
+
+        }
+        targets = Array.from(document.getElementsByClassName('target'));
+        console.log(targets)
+    }
+
+}
+
+// Event listener for the decrease targets button
+decreaseTargetsBtn.addEventListener('click', decreaseTargets);
+
+// Event listener for the increase targets button
+increaseTargetsBtn.addEventListener('click', increaseTargets);
