cc196c02ba04d0914f8461f0cdd82a94a1dd3af3
YourPuppeteer <mr.dovakin1998@mail.ru>
2023-06-22 17:08:08 +0400

    Added top 10 results

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const scoreContainer = document.getElementById('score-container')
 let targetArray = Array.from(targets);
 
 let score = 0;
-let remainingTime = 5;
+let remainingTime = 60;
 let isGameStarted = false;
 let countdownInterval;
 
@@ -145,11 +145,20 @@ function endGame() {
     });
     scoreContainer.style.display = 'block';
 
+    const currentScore = {
+        hits: score,
+        timestamp: Date.now()
+    };
+    saveScore(currentScore);
+
+    // Display the updated top scores
+    displayTopScores();
+
 }
 
 function restartGame() {
     score = 0;
-    remainingTime = 5;
+    remainingTime = 60;
     finalScore.textContent = '';
     startButton.style.display = 'block';
 
@@ -277,3 +286,74 @@ decreaseTargetsBtn.addEventListener('click', decreaseTargets);
 
 // Event listener for the increase targets button
 increaseTargetsBtn.addEventListener('click', increaseTargets);
+
+
+//Top 10 results
+
+// Load scores from local storage or initialize an empty array
+let scores = JSON.parse(localStorage.getItem('scores')) || [];
+
+// Function to save the score
+function saveScore(score) {
+    scores.push(score);
+    scores.sort((a, b) => b.hits - a.hits); // Sort scores in descending order
+    localStorage.setItem('scores', JSON.stringify(scores));
+}
+
+// Function to display the top scores
+function displayTopScores() {
+    const scoreList = document.getElementById('score-list');
+    scoreList.innerHTML = ''; // Clear the existing list
+
+    scores.slice(0, 10).forEach((score, index) => {
+        const listItem = document.createElement('li');
+
+        const scoreDiv = document.createElement('div');
+        scoreDiv.textContent = `Hits: ${score.hits}`;
+        scoreDiv.id = 'score-div'; // Assign the id "score-div"
+        listItem.appendChild(scoreDiv);
+
+        const dateDiv = document.createElement('div');
+        dateDiv.textContent = getFormattedDate(score.timestamp);
+        dateDiv.id = 'date-div'; // Assign the id "date-div"
+        listItem.appendChild(dateDiv);
+
+        scoreList.appendChild(listItem);
+    });
+}
+
+// Function to format the date
+function getFormattedDate(timestamp) {
+    const currentDate = new Date();
+    const targetDate = new Date(timestamp);
+    const timeDiff = Math.abs(currentDate - targetDate);
+    const minute = 60 * 1000;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+    const month = 30 * day;
+    const year = 365 * day;
+
+    if (timeDiff < minute) {
+        return "less than a minute ago";
+    } else if (timeDiff < hour) {
+        const minutes = Math.floor(timeDiff / minute);
+        return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+    } else if (timeDiff < day) {
+        const hours = Math.floor(timeDiff / hour);
+        return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } else if (timeDiff < month) {
+        const days = Math.floor(timeDiff / day);
+        return `${days} day${days > 1 ? 's' : ''} ago`;
+    } else if (timeDiff < year) {
+        const months = Math.floor(timeDiff / month);
+        return `${months} month${months > 1 ? 's' : ''} ago`;
+    } else {
+        const years = Math.floor(timeDiff / year);
+        return `${years} year${years > 1 ? 's' : ''} ago`;
+    }
+}
+
+
+
+// Display the top scores
+displayTopScores();
